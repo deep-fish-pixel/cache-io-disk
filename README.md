972462964c530b55f9a5d3bd23306ef341585fb3
wei.ma1 <wei.ma1@nio.com>
2021-06-03 07:48:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# cache-io-disk
+cache read and write file 
+
+### Install
+Install with npm:
+
+`$ npm install --save cache-io-disk`
+
+### Uses
+
+```
+readFile(file).then();
+writeFile(file).then();
+```
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const fse = require('fs-extra');
+const { error } = require('console-log-cmd');
+
+const cacheFileMap = new Map();
+
+/**
+ * 获取文件内容并缓存
+ * @param file
+ * @param noCache
+ * @returns {Promise<boolean|any>|Promise<T>}
+ */
+function readFile(file, noCache) {
+  const content = noCache ? false : cacheFileMap.get(file);
+  if (content) {
+    return Promise.resolve(content);
+  } else {
+    const content = fse.readFileSync(file, 'utf8');
+    // 缓存文件
+    cacheFileMap.set(file, content);
+    return Promise.resolve(content).then((content) => {
+      return content;
+    }).catch((e) => {
+      error(`[读取文件失败]不存在该文件: ${file}`);
+      return false;
+    });
+  }
+}
+/**
+ * 写入文件内容并缓存
+ * @param file
+ * @param content
+ * @returns {Promise<T>}
+ */
+function writeFile(file, content) {
+  if(fse.existsSync(file)){
+    const originalContent = fse.readFileSync(file, 'utf8');
+    // 解决重复写入导致编辑器重新加载
+    if (originalContent === content) {
+      return Promise.resolve(content);
+    }
+  }
+  // 先缓存内容
+  cacheFileMap.set(file, content);
+  fse.writeFileSync(file, content)
+  return Promise.resolve(content).catch((e) => {
+    error(`[写入文件失败] ${file} ${e.message}`);
+    return false;
+  })
+}
+
+module.exports = {
+  readFile,
+  writeFile,
+};
